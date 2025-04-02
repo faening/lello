aba926d032a968e18d7e7ca112b4296f0e44d545
faening <cmorais58@gmail.com>
2025-04-01 21:24:18 -0300

    chore: add app init

---
+++ app/src/main/java/io/github/faening/lello/LelloApplication.kt
@@ -0,0 +1,8 @@
+package io.github.faening.lello
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@Suppress("SpellCheckingInspection")
+@HiltAndroidApp
+class LelloApplication : Application()
