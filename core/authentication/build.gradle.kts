bfd3b4ae145defae9fa3bdae67696c4b00d5e4fc
faening <cmorais58@gmail.com>
2025-04-12 14:53:18 -0300

    fix: apply internal plugin onto auth

--- core/authentication/build.gradle.kts
+++ core/authentication/build.gradle.kts
@@ -1,50 +1,23 @@
 plugins {
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.ksp)
+    alias(libs.plugins.lello.android.library)
     alias(libs.plugins.lello.hilt)
-
 }
 
 android {
     namespace = "io.github.faening.lello.core.authentication"
-    compileSdk = 35
-
-    defaultConfig {
-        minSdk = 26
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
 }
 
 dependencies {
-    api(project(":core:domain"))
-    api(project(":core:model"))
+    api(projects.core.domain)
+    api(projects.core.model)
 
-    // Kotlin Coroutines
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.kotlinx.coroutines.play.services)
 
-    // Firebase
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.authentication)
 
-    // Unit Tests
     testImplementation(libs.junit)
     testImplementation(libs.kotlinx.coroutines.test)
 }
