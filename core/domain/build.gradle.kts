85190ceb3cfc80b9fcac08b60b9a1ce52ab13397
faening <cmorais58@gmail.com>
2025-04-12 14:59:10 -0300

    fix: domain module

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -1,42 +1,14 @@
 plugins {
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.lello.android.library)
     alias(libs.plugins.lello.hilt)
 }
 
 android {
     namespace = "io.github.faening.lello.core.domain"
-    compileSdk = 35
-
-    defaultConfig {
-        minSdk = 26
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
 }
 
 dependencies {
-    api(project(":core:model"))
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-
-    // Navigation
-    implementation(libs.androidx.navigation.compose)
+    api(projects.core.model)
 
     testImplementation(libs.junit)
 }
