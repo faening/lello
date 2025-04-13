c90cdb9f0271694646c7bd314e9912a0b67353f4
faening <cmorais58@gmail.com>
2025-04-13 13:13:23 -0300

    feat: add authentication feature module

---
+++ feature/authentication/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    alias(libs.plugins.lello.android.feature)
+    alias(libs.plugins.lello.android.library.compose.ui)
+}
+android {
+    namespace = "io.github.faening.lello.feature.authentication"
+}
+
+dependencies {
+    api(projects.core.authentication)
+    api(projects.core.domain)
+    api(projects.core.model)
+}
