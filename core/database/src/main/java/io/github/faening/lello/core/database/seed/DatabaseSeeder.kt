82147d32f1b3ca44c5116b0b879ed17e247e1a42
faening <cmorais58@gmail.com>
2025-05-03 23:39:34 -0300

    LEL-012 refactor: database seeding to simplify emotion and journal population

--- core/database/src/main/java/io/github/faening/lello/core/database/seed/DatabaseSeeder.kt
+++ core/database/src/main/java/io/github/faening/lello/core/database/seed/DatabaseSeeder.kt
@@ -2,7 +2,6 @@ package io.github.faening.lello.core.database.seed
 
 import android.util.Log
 import androidx.sqlite.db.SupportSQLiteDatabase
-import io.github.faening.lello.core.database.model.ClimateEntity
 
 /**
  * Classe responsável por centralizar a população de dados iniciais no banco de dados.
@@ -25,9 +24,6 @@ internal object DatabaseSeeder {
         Log.d(TAG, "Processo de seed do banco de dados concluído com sucesso")
     }
 
-    /**
-     * Popula a tabela 'climates' com dados iniciais
-     */
     fun seedClimates(db: SupportSQLiteDatabase) {
         for (climate in ClimateSeed.data) {
             db.execSQL(
@@ -44,19 +40,30 @@ internal object DatabaseSeeder {
         }
     }
 
-    /**
-     * Popula a tabela 'journals' com dados iniciais
-     */
-    fun seedJournals(db: SupportSQLiteDatabase) {
-        Log.d(TAG, "Populando tabela 'journals' com ${JournalSeed.data.size} registros")
+    fun seedEmotions(db: SupportSQLiteDatabase) {
+        for (emotion in EmotionSeed.data) {
+            db.execSQL(
+                sql = """
+                        INSERT INTO emotions (word, blocked, active)
+                        VALUES (?, ?, ?)
+                    """.trimIndent(),
+                bindArgs = arrayOf(
+                    emotion.word,
+                    if (emotion.blocked) 1 else 0,
+                    if (emotion.active) 1 else 0
+                )
+            )
+        }
+    }
 
+    fun seedJournals(db: SupportSQLiteDatabase) {
         for (journal in JournalSeed.data) {
             db.execSQL(
-                """
-                    INSERT INTO journals (name, short_description, long_description, blocked, active)
-                    VALUES (?, ?, ?, ?, ?)
-                """.trimIndent(),
-                arrayOf(
+                sql = """
+                        INSERT INTO journals (name, short_description, long_description, blocked, active)
+                        VALUES (?, ?, ?, ?, ?)
+                    """.trimIndent(),
+                bindArgs = arrayOf(
                     journal.name,
                     journal.shortDescription,
                     journal.longDescription,
@@ -65,30 +72,5 @@ internal object DatabaseSeeder {
                 )
             )
         }
-
-        Log.d(TAG, "Tabela 'journals' populada com sucesso")
-    }
-
-    /**
-     * Popula a tabela 'emotions' com dados iniciais
-     */
-    fun seedEmotions(db: SupportSQLiteDatabase) {
-        Log.d(TAG, "Populando tabela 'emotions' com ${EmotionSeed.data.size} registros")
-
-        for (emotion in EmotionSeed.data) {
-            db.execSQL(
-                """
-                    INSERT INTO emotions (word, blocked, active)
-                    VALUES (?, ?, ?)
-                """.trimIndent(),
-                arrayOf(
-                    emotion.word,
-                    if (emotion.blocked) 1 else 0,
-                    if (emotion.active) 1 else 0
-                )
-            )
-        }
-
-        Log.d(TAG, "Tabela 'emotions' populada com sucesso")
     }
 }
--- core/database/src/main/java/io/github/faening/lello/core/database/seed/EmotionSeed.kt
+++ core/database/src/main/java/io/github/faening/lello/core/database/seed/EmotionSeed.kt
@@ -2,163 +2,33 @@ package io.github.faening.lello.core.database.seed
 
 import io.github.faening.lello.core.database.model.EmotionEntity
 
-internal object EmotionSeed: Seed<EmotionEntity> {
+internal object EmotionSeed : Seed<EmotionEntity> {
     override val data = listOf(
-        EmotionEntity(
-            id = 0,
-            word = "Amado",
-            blocked = true,
-            active = true
-        ),
-        EmotionEntity(
-            id = 1,
-            word = "Apático",
-            blocked = true,
-            active = true
-        ),
-        EmotionEntity(
-            id = 2,
-            word = "Confiante",
-            blocked = true,
-            active = true
-        ),
-        EmotionEntity(
-            id = 3,
-            word = "Confuso",
-            blocked = true,
-            active = true
-        ),
-        EmotionEntity(
-            id = 4,
-            word = "Culpado",
-            blocked = true,
-            active = true
-        ),
-        EmotionEntity(
-            id = 5,
-            word = "Decepcionado",
-            blocked = true,
-            active = true
-        ),
-        EmotionEntity(
-            id = 6,
-            word = "Desanimado",
-            blocked = true,
-            active = true
-        ),
-        EmotionEntity(
-            id = 7,
-            word = "Desconfiado",
-            blocked = true,
-            active = true
-        ),
-        EmotionEntity(
-            id = 8,
-            word = "Entediado",
-            blocked = true,
-            active = true
-        ),
-        EmotionEntity(
-            id = 9,
-            word = "Enjoado",
-            blocked = true,
-            active = true
-        ),
-        EmotionEntity(
-            id = 10,
-            word = "Estressado",
-            blocked = true,
-            active = true
-        ),
-        EmotionEntity(
-            id = 11,
-            word = "Frustrado",
-            blocked = true,
-            active = true
-        ),
-        EmotionEntity(
-            id = 12,
-            word = "Grato",
-            blocked = true,
-            active = true
-        ),
-        EmotionEntity(
-            id = 13,
-            word = "Incomodado",
-            blocked = true,
-            active = true
-        ),
-        EmotionEntity(
-            id = 14,
-            word = "Inseguro",
-            blocked = true,
-            active = true
-        ),
-        EmotionEntity(
-            id = 15,
-            word = "Irritado",
-            blocked = true,
-            active = true
-        ),
-        EmotionEntity(
-            id = 16,
-            word = "Melancólico",
-            blocked = true,
-            active = true
-        ),
-        EmotionEntity(
-            id = 17,
-            word = "Nervoso",
-            blocked = true,
-            active = true
-        ),
-        EmotionEntity(
-            id = 18,
-            word = "Otimista",
-            blocked = true,
-            active = true
-        ),
-        EmotionEntity(
-            id = 19,
-            word = "Orgulhoso",
-            blocked = true,
-            active = true
-        ),
-        EmotionEntity(
-            id = 20,
-            word = "Preocupado",
-            blocked = true,
-            active = true
-        ),
-        EmotionEntity(
-            id = 21,
-            word = "Sentimental",
-            blocked = true,
-            active = true
-        ),
-        EmotionEntity(
-            id = 22,
-            word = "Solitário",
-            blocked = true,
-            active = true
-        ),
-        EmotionEntity(
-            id = 23,
-            word = "Stressado",
-            blocked = true,
-            active = true
-        ),
-        EmotionEntity(
-            id = 24,
-            word = "Tímido",
-            blocked = true,
-            active = true
-        ),
-        EmotionEntity(
-            id = 25,
-            word = "Triste",
-            blocked = true,
-            active = true
-        )
+        EmotionEntity(id = 1, word = "Amado", blocked = true, active = true),
+        EmotionEntity(id = 2, word = "Apático", blocked = true, active = true),
+        EmotionEntity(id = 3, word = "Confiante", blocked = true, active = true),
+        EmotionEntity(id = 4, word = "Confuso", blocked = true, active = true),
+        EmotionEntity(id = 5, word = "Culpado", blocked = true, active = true),
+        EmotionEntity(id = 6, word = "Decepcionado", blocked = true, active = true),
+        EmotionEntity(id = 7, word = "Desanimado", blocked = true, active = true),
+        EmotionEntity(id = 8, word = "Desconfiado", blocked = true, active = true),
+        EmotionEntity(id = 9, word = "Entediado", blocked = true, active = true),
+        EmotionEntity(id = 10, word = "Enjoado", blocked = true, active = true),
+        EmotionEntity(id = 11, word = "Estressado", blocked = true, active = true),
+        EmotionEntity(id = 12, word = "Frustrado", blocked = true, active = true),
+        EmotionEntity(id = 13, word = "Grato", blocked = true, active = true),
+        EmotionEntity(id = 14, word = "Incomodado", blocked = true, active = true),
+        EmotionEntity(id = 15, word = "Inseguro", blocked = true, active = true),
+        EmotionEntity(id = 16, word = "Irritado", blocked = true, active = true),
+        EmotionEntity(id = 17, word = "Melancólico", blocked = true, active = true),
+        EmotionEntity(id = 18, word = "Nervoso", blocked = true, active = true),
+        EmotionEntity(id = 19, word = "Otimista", blocked = true, active = true),
+        EmotionEntity(id = 20, word = "Orgulhoso", blocked = true, active = true),
+        EmotionEntity(id = 21, word = "Preocupado", blocked = true, active = true),
+        EmotionEntity(id = 22, word = "Sentimental", blocked = true, active = true),
+        EmotionEntity(id = 23, word = "Solitário", blocked = true, active = true),
+        EmotionEntity(id = 24, word = "Stressado", blocked = true, active = true),
+        EmotionEntity(id = 25, word = "Tímido", blocked = true, active = true),
+        EmotionEntity(id = 26, word = "Triste", blocked = true, active = true)
     )
 }
