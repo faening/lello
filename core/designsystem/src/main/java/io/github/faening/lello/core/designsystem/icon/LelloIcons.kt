c6f04334ca6612cdd0307155c83f7ba668e9d544
faening <cmorais58@gmail.com>
2025-05-16 22:09:59 -0300

    LEL-014 feat: add capybara mood icons
    Adds new SVG icons for different capybara moods:
    - `ic_capy_bad.xml`
    - `ic_capy_excelent.xml`
    - `ic_capy_happy.xml`
    - `ic_capy_horrible.xml`
    - `ic_capy_normal.xml`
    
    Updates `LelloIcons.kt` to include these new icons and removes unused icons related to "Diary" and "Dashboard".

--- core/designsystem/src/main/java/io/github/faening/lello/core/designsystem/icon/LelloIcons.kt
+++ core/designsystem/src/main/java/io/github/faening/lello/core/designsystem/icon/LelloIcons.kt
@@ -1,22 +1,14 @@
 package io.github.faening.lello.core.designsystem.icon
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.PieChart
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Inventory2
-import androidx.compose.material.icons.outlined.LibraryBooks
 import androidx.compose.material.icons.outlined.Person
-import androidx.compose.material.icons.outlined.PieChartOutline
-import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material.icons.rounded.Add
-import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material.icons.rounded.Favorite
 import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material.icons.rounded.Inventory2
-import androidx.compose.material.icons.rounded.LibraryBooks
 import androidx.compose.material.icons.rounded.MoreVert
 import androidx.compose.material.icons.rounded.Person
-import androidx.compose.material.icons.rounded.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
@@ -26,23 +18,14 @@ object LelloIcons {
     // Menu
     val Home = Icons.Outlined.Home
     val HomeBorder = Icons.Rounded.Home
-    val Diary = Icons.Outlined.LibraryBooks
-    val DiaryBorder = Icons.Rounded.LibraryBooks
     val Achievements = Icons.Outlined.Inventory2
     val AchievementsBorder = Icons.Rounded.Inventory2
-    val Dashboard = Icons.Outlined.PieChartOutline
-    val DashboardBorder = Icons.Filled.PieChart
     val Profile = Icons.Outlined.Person
     val ProfileBorder = Icons.Rounded.Person
 
     // TopAppBar
-    val ArrowBack = Icons.Rounded.ArrowBack
-    val Search = Icons.Rounded.Search
     val Favorite = Icons.Rounded.Favorite
     val MoreVert = Icons.Rounded.MoreVert
-    val Settings = Icons.Outlined.Settings
-
-    val Add = Icons.Rounded.Add
 
     // Carregar ícones SVG
     @Composable
