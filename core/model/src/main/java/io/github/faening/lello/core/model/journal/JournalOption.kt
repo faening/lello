1a8f11e7253c20fc68e150ca1d4b0395123194cf
Arthur Cesar <38919735+faening@users.noreply.github.com>
2025-06-08 13:04:01 -0300

    feat(model): add JournalOption interface

---
+++ core/model/src/main/java/io/github/faening/lello/core/model/journal/JournalOption.kt
@@ -0,0 +1,10 @@
+package io.github.faening.lello.core.model.journal
+
+import java.io.Serializable
+
+interface JournalOption : Serializable {
+    val id: Int?
+    val description: String
+    val blocked: Boolean
+    val active: Boolean
+}
