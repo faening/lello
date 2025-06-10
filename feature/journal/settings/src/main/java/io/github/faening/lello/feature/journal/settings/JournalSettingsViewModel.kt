aed1f016d306ed9f039c98e97239047cc7d30f02
Arthur Cesar <38919735+faening@users.noreply.github.com>
2025-06-10 19:55:54 -0300

    Add location options settings screen

--- feature/journal/settings/src/main/java/io/github/faening/lello/feature/journal/settings/JournalSettingsViewModel.kt
+++ feature/journal/settings/src/main/java/io/github/faening/lello/feature/journal/settings/JournalSettingsViewModel.kt
@@ -5,8 +5,10 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.github.faening.lello.core.domain.usecase.options.ClimateOptionUseCase
 import io.github.faening.lello.core.domain.usecase.options.EmotionOptionUseCase
+import io.github.faening.lello.core.domain.usecase.options.LocationOptionUseCase
 import io.github.faening.lello.core.model.journal.ClimateOption
 import io.github.faening.lello.core.model.journal.EmotionOption
+import io.github.faening.lello.core.model.journal.LocationOption
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -15,7 +17,8 @@ import javax.inject.Inject
 @HiltViewModel
 class JournalSettingsViewModel @Inject constructor(
     emotionOptionUseCase: EmotionOptionUseCase,
-    climateOptionUseCase: ClimateOptionUseCase
+    climateOptionUseCase: ClimateOptionUseCase,
+    locationOptionUseCase: LocationOptionUseCase,
 ) : ViewModel() {
 
     private val _emotionOptions = MutableStateFlow<List<EmotionOption>>(emptyList())
@@ -24,12 +27,18 @@ class JournalSettingsViewModel @Inject constructor(
     private val _climateOptions = MutableStateFlow<List<ClimateOption>>(emptyList())
     val climateOptions: StateFlow<List<ClimateOption>> = _climateOptions
 
+    private val _locationOptions = MutableStateFlow<List<LocationOption>>(emptyList())
+    val locationOptions: StateFlow<List<LocationOption>> = _locationOptions
+
     init {
         viewModelScope.launch {
             emotionOptionUseCase.getAll().collect { _emotionOptions.value = it }
         }
         viewModelScope.launch {
             climateOptionUseCase.getAll().collect { _climateOptions.value = it }
+        }
+        viewModelScope.launch {
+            locationOptionUseCase.getAll().collect { _locationOptions.value = it }
         }
     }
 
@@ -41,6 +50,12 @@ class JournalSettingsViewModel @Inject constructor(
 
     fun toggleClimateOption(option: ClimateOption, active: Boolean) {
         _climateOptions.value = _climateOptions.value.map {
+            if (it.id == option.id) it.copy(active = active) else it
+        }
+    }
+
+    fun toggleLocationOption(option: LocationOption, active: Boolean) {
+        _locationOptions.value = _locationOptions.value.map {
             if (it.id == option.id) it.copy(active = active) else it
         }
     }
