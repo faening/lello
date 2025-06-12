f847eb472dc35e523614dde6c84064b2f6a9f89f
Arthur Cesar <38919735+faening@users.noreply.github.com>
2025-06-12 13:34:24 -0300

    feat: implement option repositories and usecases

---
+++ core/domain/src/main/java/io/github/faening/lello/core/domain/usecase/options/DosageFormOptionUseCase.kt
@@ -0,0 +1,55 @@
+package io.github.faening.lello.core.domain.usecase.options
+
+import io.github.faening.lello.core.domain.repository.OptionResources
+import io.github.faening.lello.core.domain.util.capitalizeFirst
+import io.github.faening.lello.core.domain.util.validateDescription
+import io.github.faening.lello.core.domain.util.validateId
+import io.github.faening.lello.core.domain.util.validateNotBlocked
+import io.github.faening.lello.core.model.journal.DosageFormOption
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class DosageFormOptionUseCase @Inject constructor(
+    private val repository: OptionResources<DosageFormOption>
+) {
+
+    fun getAll(
+        useBlockedFilter: Boolean = false,
+        isBlocked: Boolean = true,
+        useActiveFilter: Boolean = false,
+        isActive: Boolean = true
+    ): Flow<List<DosageFormOption>> {
+        return repository.getAll(useBlockedFilter, isBlocked, useActiveFilter, isActive)
+    }
+
+    fun getById(id: Long): Flow<DosageFormOption>? {
+        id.validateId()
+        return repository.getById(id)
+    }
+
+    suspend fun save(vararg items: DosageFormOption) {
+        val formattedItems = items.map { item ->
+            item.description.validateDescription()
+            item.copy(description = item.description.capitalizeFirst())
+        }
+        formattedItems.forEach { item -> repository.insert(item) }
+    }
+
+    suspend fun update(vararg items: DosageFormOption) {
+        val formattedItems = items.map { item ->
+            item.blocked.validateNotBlocked()
+            item.id.validateId()
+            item.description.validateDescription()
+            item.copy(description = item.description.capitalizeFirst())
+        }
+        formattedItems.forEach { item -> repository.update(item) }
+    }
+
+    suspend fun delete(vararg items: DosageFormOption) {
+        items.forEach { item ->
+            item.blocked.validateNotBlocked()
+            item.id.validateId()
+        }
+        items.forEach { item -> repository.delete(item) }
+    }
+}
