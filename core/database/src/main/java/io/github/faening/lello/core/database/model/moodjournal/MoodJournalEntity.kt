ce488c38706536280e5f1ca7ccce797bfa590454
faening <cmorais58@gmail.com>
2025-06-13 12:44:40 -0300

    LEL-015 refactor: add toModel and toEntity extension functions for MoodJournal

--- core/database/src/main/java/io/github/faening/lello/core/database/model/moodjournal/MoodJournalEntity.kt
+++ core/database/src/main/java/io/github/faening/lello/core/database/model/moodjournal/MoodJournalEntity.kt
@@ -2,6 +2,7 @@ package io.github.faening.lello.core.database.model.moodjournal
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import io.github.faening.lello.core.model.journal.MoodJournal
 import io.github.faening.lello.core.model.journal.MoodType
 import java.util.Date
 
@@ -12,3 +13,26 @@ data class MoodJournalEntity(
     val mood: MoodType,
     val reflection: String? = null
 )
+
+fun MoodJournalEntity.toModel(): MoodJournal {
+    return MoodJournal(
+        id = moodJournalId,
+        date = date,
+        mood = mood,
+        reflection = reflection,
+        emotionOptions = emptyList(),
+        climateOptions = emptyList(),
+        locationOptions = emptyList(),
+        socialOptions = emptyList(),
+        healthOptions = emptyList()
+    )
+}
+
+fun MoodJournal.toEntity(): MoodJournalEntity {
+    return MoodJournalEntity(
+        moodJournalId = id ?: 0L,
+        date = date,
+        mood = mood,
+        reflection = reflection
+    )
+}
