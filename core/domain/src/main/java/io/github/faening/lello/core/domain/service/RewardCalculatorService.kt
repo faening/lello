85693f66e0f0f5018d2643abc4e9735b6a7d880e
faening <cmorais58@gmail.com>
2025-06-23 23:53:23 -0300

    LEL-023 feat(core/domain/meal): add rewards on save journal

--- core/domain/src/main/java/io/github/faening/lello/core/domain/service/RewardCalculatorService.kt
+++ core/domain/src/main/java/io/github/faening/lello/core/domain/service/RewardCalculatorService.kt
@@ -1,10 +1,25 @@
 package io.github.faening.lello.core.domain.service
 
+import io.github.faening.lello.core.model.journal.MealJournal
 import io.github.faening.lello.core.model.journal.MoodJournal
 import io.github.faening.lello.core.model.reward.RewardPoints
 import javax.inject.Inject
 
 class RewardCalculatorService @Inject constructor() {
+
+    fun calculateForMealJournal(mealJournal: MealJournal): Int {
+        val rewardPoints = RewardPoints.MEAL_JOURNAL
+        var reward = rewardPoints.basePoints
+
+        if (mealJournal.mealTime > 0) reward += rewardPoints.extraPoints
+        if (mealJournal.foodOptions.isNotEmpty()) reward += rewardPoints.extraPoints
+        if (mealJournal.portionOptions.isNotEmpty()) reward += rewardPoints.extraPoints
+        if (mealJournal.locationOptions.isNotEmpty()) reward += rewardPoints.extraPoints
+        if (mealJournal.socialOptions.isNotEmpty()) reward += rewardPoints.extraPoints
+
+        return reward
+    }
+
     fun calculateForMoodJournal(moodJournal: MoodJournal): Int {
         val rewardPoints = RewardPoints.MOOD_JOURNAL
         var reward = rewardPoints.basePoints
