11146473df6585e6cb6a94341052d88f30ac7d92
faening <cmorais58@gmail.com>
2025-08-10 20:49:45 -0300

    LEL-026 refactor: add centralized icon manager

--- core/designsystem/src/main/java/io/github/faening/lello/core/designsystem/icon/LelloIcons.kt
+++ core/designsystem/src/main/java/io/github/faening/lello/core/designsystem/icon/LelloIcons.kt
@@ -2,35 +2,39 @@ package io.github.faening.lello.core.designsystem.icon
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Home
-import androidx.compose.material.icons.outlined.Inventory2
-import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Favorite
-import androidx.compose.material.icons.rounded.Home
-import androidx.compose.material.icons.rounded.Inventory2
 import androidx.compose.material.icons.rounded.MoreVert
-import androidx.compose.material.icons.rounded.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import io.github.faening.lello.core.designsystem.R as designsystemR
 
 @Suppress("DEPRECATION")
 object LelloIcons {
     // Menu
     val Home = Icons.Outlined.Home
-    val HomeBorder = Icons.Rounded.Home
-    val Achievements = Icons.Outlined.Inventory2
-    val AchievementsBorder = Icons.Rounded.Inventory2
-    val Profile = Icons.Outlined.Person
-    val ProfileBorder = Icons.Rounded.Person
 
     // TopAppBar
     val Favorite = Icons.Rounded.Favorite
     val MoreVert = Icons.Rounded.MoreVert
-
     val Add = Icons.Rounded.Add
 
-    // Carregar ícones SVG
+    val Logo = LelloIcon(designsystemR.drawable.ic_lello_logo)
+
+    object Outlined {
+        val Mail = LelloIcon(designsystemR.drawable.ic_mail_outlined)
+    }
+
+    object Filled {
+        val Google = LelloIcon(designsystemR.drawable.ic_google_filled)
+    }
+
     @Composable
     fun customIcon(id: Int): ImageVector = ImageVector.vectorResource(id = id)
+}
+
+data class LelloIcon(val resId: Int) {
+    val imageVector: ImageVector
+        @Composable get() = ImageVector.vectorResource(id = resId)
 }
