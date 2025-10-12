7f7c660de6e3fdc2fd94cf8181d260feea2a2ffd
faening <cmorais58@gmail.com>
2025-10-12 20:43:38 -0300

    LEL-028 chore(profile): remove profile feature module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,6 @@ dependencies {
     api(projects.feature.onboarding)
     api(projects.feature.achievement)
     api(projects.feature.medication)
-    api(projects.feature.profile)
     api(projects.feature.settings)
     api(projects.feature.journal.meal)
     api(projects.feature.journal.medication)
