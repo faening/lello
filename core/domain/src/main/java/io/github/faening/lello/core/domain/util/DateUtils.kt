ad37df1fcda804559a702ff0f312843389a4e990
faening <cmorais58@gmail.com>
2025-11-07 00:19:43 -0300

    LEL-031 feat(dateutils): add function to convert LocalTime to epoch millis

--- core/domain/src/main/java/io/github/faening/lello/core/domain/util/DateUtils.kt
+++ core/domain/src/main/java/io/github/faening/lello/core/domain/util/DateUtils.kt
@@ -3,6 +3,7 @@ package io.github.faening.lello.core.domain.util
 import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.LocalTime
 import java.time.ZoneId
 
 fun Long.toInstant(): Instant = Instant.ofEpochMilli(this)
@@ -18,4 +19,16 @@ fun LocalDateTime.toEpochMillis(zone: ZoneId = ZoneId.systemDefault()): Long =
 fun Long.isSameDay(date: LocalDate): Boolean {
     val thisDate = Instant.ofEpochMilli(this).atZone(ZoneId.systemDefault()).toLocalDate()
     return thisDate == date
+}
+
+/**
+ * Converte um [LocalTime] para epoch millis em uma data específica.
+ *
+ * @param date A data para a qual o tempo será aplicado.
+ * @param zone O fuso horário a ser usado. Padrão é o fuso
+ * @return epoch millis correspondente ao [LocalTime] na [date] especificada.
+ */
+fun LocalTime.toEpochMillisOnDate(date: LocalDate, zone: ZoneId = ZoneId.systemDefault()): Long {
+    val dateTime = LocalDateTime.of(date, this)
+    return dateTime.atZone(zone).toInstant().toEpochMilli()
 }
